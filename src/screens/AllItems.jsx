74146160e3fa7b75669ecf03772402130da5a254
sharique ahmad <ahmedsharique250@gmail.com>
2025-09-13 12:37:20 +0530

    render items

--- src/screens/AllItems.jsx
+++ src/screens/AllItems.jsx
@@ -1,14 +1,57 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList } from 'react-native'
 
-const AllItems = () => {
+const AllItems = ({ data }) => {
   return (
     <View>
-      <Text>AllItems</Text>
+      <View style={styles.container}>
+        <Text style={styles.headingtxt}>All Items</Text>
+        <Text style={styles.headingtxt}>Quantity</Text>
+      </View>
+      <FlatList
+        data={data}
+        // key extractor is used to extract the key or unique id form the items
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.itemcontainer}>
+            <Text style={styles.itemtxt}>{item.name}</Text>
+            <Text style={styles.itemtxt}>{item.stock}</Text>
+          </View>
+        )}
+
+      />
+      {/* here i have used data as a prop which collected the mock data i have created and add it in the flatlist*/}
+
+
+
     </View>
   )
 }
 
 export default AllItems
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+
+  },
+  headingtxt: {
+    fontWeight: "500",
+    fontSize: 14,
+    paddingVertical: 10,
+    paddingHorizontal: 10
+  },
+  itemcontainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  itemtxt: {
+    fontWeight: "400",
+    fontSize: 13,
+    // gap:10
+
+  }
+})
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -6,7 +6,72 @@ import LowStock from './LowStock'
 import CreateScreen from './createScreen' 
 
 const HomeScreen = () => {
+   const data=
+    [
+  {
+    id:1,
+    name: "iPhone 14",
+    stock: 20,
+    price: 80000
+  },
+  {
+    id:2,
+    name: "Samsung Galaxy S23",
+    stock: 15,
+    price: 75000
+  },
+  {
+    id:3,
+    name: "OnePlus 11",
+    stock: 25,
+    price: 60000
+  },
+  {
+    id:4,
+    name: "Google Pixel 7 Pro",
+    stock: 10,
+    price: 70000
+  },
+  {
+    id:5,
+    name: "Xiaomi 13 Pro",
+    stock: 30,
+    price: 55000
+  },
+  {
+    id:6,
+    name: "Realme GT Neo 5",
+    stock: 18,
+    price: 42000
+  },
+  {
+    id:7,
+    name: "Vivo X90 Pro",
+    stock: 22,
+    price: 63000
+  },
+  {
+    id:8,
+    name: "Oppo Find X6",
+    stock: 12,
+    price: 58000
+  },
+  {
+    id:9,
+    name: "Asus ROG Phone 7",
+    stock: 8,
+    price: 72000
+  },
+  {
+    id:10,
+    name: "Nothing Phone (2)",
+    stock: 16,
+    price: 48000
+  }
+]
 
+   
+   
   const [view, setview] = useState(0)
   return (
     <View style={styles.container}>
@@ -16,13 +81,13 @@ const HomeScreen = () => {
           <Text style={styles.btntxt}>All items</Text>
         </Pressable>
         <Pressable style={[styles.btn,view===1?{backgroundColor:"slateblue"}:null]} onPress={()=>setview(1)}>
-          <Text style={styles.btntxt}>Low stock</Text>
+          <Text style={[styles.btntxt,view===1?{color:"white"}:null]}>Low stock</Text>
         </Pressable>
          <Pressable style={[styles.btn,view===2?{backgroundColor:"slateblue"}:null]} onPress={()=>setview(2)}>
-          <Text style={styles.btntxt}>Create items</Text>
+          <Text style={[styles.btntxt,view===2?{color:"white"}:null]}>Create items</Text>
         </Pressable>
       </View>
-      {view===0 &&<AllItems/>}
+      {view===0 &&<AllItems data={data}/>}
       {view===1 &&<LowStock/>}
       {view===2 &&<CreateScreen/>}
     </View>
